202c77398b594a627560015a154818e098050351
pichuzhkinaen <88-katerina@mail.ru>
2024-01-23 17:12:59 +0300

    chore: buy product

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,15 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | null = null;
+    @Output() readonly buy = new EventEmitter();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
-
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+        this.buy.emit(event);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return !!(this.product && this.product.rating >= starIndex);
     }
 }
